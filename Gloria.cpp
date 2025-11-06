403468db4766be67a534181e197ac0aab36668ee
LeandroFurnas <furnas2008@gmail.com>
2025-11-06 20:36:48 -0100

    Refactor game board and add CasaTipo enum

--- Gloria.cpp
+++ Gloria.cpp
@@ -4,30 +4,65 @@
 using namespace std;
 
 const int TAMANHO_TABULEIRO = 30;
-const int CasaNormal = 0;
-const int CasaPerdeVida = 1;
-const int CasaRetorna3 = 2;
-const int CasaAvanca3 = 3;
-const int CasaJogar = 4;
-const int CasaEsperar = 5;
 
-void ColocarCasaAleatoriamente(){
-    srand(time(0));
-    for (int i = 1; i <= TAMANHO_TABULEIRO; i++) {
-        cout << "[ ]";
-        if (i % 10 == 0){ cout << "\n";
-
-     
-       
-} 
-    cout << "\n";
-}
+void MainPlayer(){
+    const string nome = "L";
+    int casa;
+    
 }
 
-void exibirCabecalho() {
-    cout << "=========================================\n";
-    cout << "          Jogo de Lançamento de Dados    \n";
-    cout << "=========================================\n";
+enum class CasaTipo
+{
+    Normal = 0,
+    PerdeVez = 1,
+    Retorna3 = 2,
+    Avanca3 = 3,
+    JogarDenovo = 4,
+    Esperar = 5
+    
+};
+
+string NomeCasa(CasaTipo c){
+    switch (c)
+    {
+    case CasaTipo::Normal: return "N ";
+    case CasaTipo::PerdeVez: return "PV";
+    case CasaTipo::Retorna3: return "R3";
+    case CasaTipo::Avanca3: return "A3";
+    case CasaTipo::JogarDenovo: return "JN";
+    case CasaTipo::Esperar: return "E ";
+        break;
+      default: return "?";
+        break;
+    }
+
+}
+
+void ColocarCasaAleatoriamente() {
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios
+
+    CasaTipo casas[TAMANHO_TABULEIRO];
+
+    // Inicializa todas as casas como normais
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        casas[i] = CasaTipo::Normal;
+    }
+
+    // Define casas especiais aleatoriamente
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        if ((i + 1) % 6 == 0) { //a cada 6
+            int tipo = (rand() % 5) + 1; // rand de 0-5
+            casas[i] = static_cast<CasaTipo>(tipo);
+        }
+
+        cout << "[" << NomeCasa(casas[i]) << "]";
+            if ((i + 1) % 10 == 0) 
+            cout << "\n";
+    }
+}
+
+void CasaJogada(CasaTipo casas){
+    
 }
 
 void menu() {
@@ -36,6 +71,9 @@ void menu() {
 }
 
 void mostrarDado(int numero) {
+    cout << "=========================================\n";
+    cout << "          Gira o dado para andares!    \n";
+    cout << "=========================================\n";
     cout << "Resultado do dado: " << numero << endl;
     cout << "=================\n";
     switch (numero) {
@@ -83,8 +121,6 @@ void dado() {
 #else
         system("clear");
 #endif
-
-        exibirCabecalho();
         menu();
 
         int numero = (rand() % 6) + 1;
@@ -99,9 +135,13 @@ void dado() {
 }
 
 int main() {
+
+    while (true)
+    {
         ColocarCasaAleatoriamente();
-    exibirCabecalho();
-    dado();
+        cin.get();
+        dado(); 
+    }
 
     return 0;
 }
